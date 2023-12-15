c8ca59357917e1e49adb6e13d4cf3f910d13e13b
Mohamed02 <emerald.mohamed@gmail.com>
2023-12-15 11:08:05 +0530

    changes

--- config/dev.js
+++ config/dev.js
@@ -5,7 +5,7 @@ module.exports = {
   // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
   // mongoURI: 'mongodb+srv://ismaildev:OnM1mjRxHHFUHwFz@cluster0.qkovk.mongodb.net/blog_everyone',
   // mongoURI: 'mongodb://ismaildev:jtR5eWDPk4lfruUS@cluster0.qkovk.mongodb.net/blog_everyone?retryWrites=true&w=majority',
-  // mongoURI: 'mongodb+srv://ismaildev:jtR5eWDPk4lfruUS@cluster0.qkovk.mongodb.net/blog_everyone',
-  mongoURI: 'mongodb://127.0.0.1:27017/blog_temp',
+  mongoURI: 'mongodb://localhost:27017/blog_everyone',
+  // mongoURI: 'mongodb://127.0.0.1:27017/blog_temp',
   cookieKey: '123123123',
 };
