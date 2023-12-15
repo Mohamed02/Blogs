08a019445eb4c736e767aa38f6214341683b3b68
Mohamed02 <emerald.mohamed@gmail.com>
2023-12-15 10:50:14 +0530

    changes

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:   
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+    '864518686358-48v6f69qcbfsssoittd9c55j2io93ks8.apps.googleusercontent.com',
+  googleClientSecret: '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',
   // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
   // mongoURI: 'mongodb+srv://ismaildev:OnM1mjRxHHFUHwFz@cluster0.qkovk.mongodb.net/blog_everyone',
   // mongoURI: 'mongodb://ismaildev:jtR5eWDPk4lfruUS@cluster0.qkovk.mongodb.net/blog_everyone?retryWrites=true&w=majority',
