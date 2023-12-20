9f249f227306596b5553026c03285c7cb122e7f4
Mohamed02 <emerald.mohamed@gmail.com>
2023-12-20 07:35:49 +0530

    changes

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,7 +5,7 @@ const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
     <h1>
-      Blogster Ismail!
+      Blogster Mohamed!
     </h1>
     Write private blogs
   </div>
