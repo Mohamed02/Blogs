3708d00e2769ad0b0921f2893a2f315683baa33e
Mohamed02 <emerald.mohamed@gmail.com>
2023-12-22 20:37:19 +0530

    changes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,15 +3,15 @@ const requireLogin = require('../middlewares/requireLogin');
 const Blog = mongoose.model('Blog');
 
 module.exports= app=>{
-    app.get('/api/blogs',requireLogin, async(req,res)=>{
-        const blogs = await Blog.find({_user: req.user.id}).then((blogs)=>{
+    app.get('/api/blogs', async(req,res)=>{
+        const blogs = await Blog.find({}).then((blogs)=>{
                 res.send(blogs);
         }).catch(err=>{
             res.send("unable to retrieve data")
         })
         
     });
-    app.post('/api/blogs', async(req,res) => {
+    app.post('/api/blogs',requireLogin, async(req,res) => {
         const { title, content } = req.body;
         const blog = new Blog({
             title,
